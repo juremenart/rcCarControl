66bfe8efc491eca23b24b461f96dfd722c0ba4c0
Jure Menart <juremenart@gmail.com>
2018-01-29 20:44:26 +0100

    added very basic rccVideoCtrl class and standalone app to access register

---
+++ sw/daemon/rcc_video_ctrl.cpp
@@ -0,0 +1,123 @@
+#include <string.h>
+#include <errno.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <iostream>
+#include <sstream>
+
+#include "rcc_video_ctrl.h"
+#include "rcc_logger.h"
+
+rccVideoCtrl::rccVideoCtrl(void) :
+    mMemFd(-1), mRegs(NULL)
+{
+    void *pagePtr;
+    long pageAddr, pageOff, pageSize = sysconf(_SC_PAGESIZE);
+
+    std::ostringstream strStream;
+    mMemFd = open("/dev/mem", O_RDWR | O_SYNC);
+    if(mMemFd < 0)
+    {
+        strStream.str(std::string());
+        strStream << "open() of /dev/mem failed: " << strerror(errno) << std::endl;
+        getLogger().error(strStream.str());
+        return;
+    }
+
+    pageAddr   = cVideoCtrlAddr & (~(pageSize-1));
+    pageOff    = cVideoCtrlAddr - pageAddr;
+    pagePtr    = mmap(NULL, sizeof(axiVideoCtrlRegs_t),
+                      PROT_READ | PROT_WRITE, MAP_SHARED,
+                      mMemFd, pageAddr);
+    if((void*)pagePtr == MAP_FAILED)
+    {
+        strStream.str(std::string());
+        strStream << "mmap() of /dev/mem failed: " << strerror(errno) << std::endl;
+        getLogger().error(strStream.str());
+        return;
+    }
+
+    mRegs = (axiVideoCtrlRegs_t *)pagePtr + pageOff;
+}
+
+rccVideoCtrl::~rccVideoCtrl(void)
+{
+    cleanup();
+}
+
+int rccVideoCtrl::cleanup(void)
+{
+    std::ostringstream strStream;
+
+    if(mRegs)
+    {
+        if(munmap((void *)mRegs, cVideoCtrlAddr) < 0)
+        {
+            strStream.str(std::string());
+            strStream << "munmap() failed: " << strerror(errno) << std::endl;
+            getLogger().error(strStream.str());
+            return -1;
+        }
+        mRegs = NULL;
+    }
+
+    if(mMemFd)
+    {
+        close(mMemFd);
+        mMemFd = -1;
+    }
+
+    return 0;
+}
+
+int rccVideoCtrl::version(void)
+{
+    if(isInitialized())
+    {
+        return mRegs->version;
+    }
+    return -1;
+}
+
+int rccVideoCtrl::writeReg(uint8_t regOffset, uint32_t regValue)
+{
+    uint32_t *addr = (uint32_t *)mRegs;
+
+    if(!isInitialized())
+    {
+        return -1;
+    }
+
+    *(addr+(regOffset>>2)) = regValue;
+    return 0;
+}
+
+uint32_t rccVideoCtrl::readReg(uint8_t regOffset)
+{
+    uint32_t val = 0xdeadbeef;
+    uint32_t *addr = (uint32_t *)mRegs;
+
+    if(!isInitialized())
+    {
+        return val;
+    }
+
+    return *(addr+(regOffset>>2));
+}
+
+void rccVideoCtrl::dumpRegs(void)
+{
+    uint32_t *addr = (uint32_t *)mRegs;
+
+    for(int i = 0; i < (int)sizeof(axiVideoCtrlRegs_t); i++)
+    {
+        std::cout << "Offset=0x" << std::hex << i
+                  << " Value=0x" << *(addr+i) << std::endl;
+    }
+
+    return;
+}
---
+++ sw/daemon/rcc_video_ctrl.h
@@ -0,0 +1,43 @@
+#ifndef __RCC_VIDEO_CTRL_H
+#define __RCC_VIDEO_CTRL_H
+
+extern "C" {
+#include "rcci_type.h"
+}
+
+
+// System control module class
+class rccVideoCtrl {
+private:
+    const uint32_t cVideoCtrlAddr   = 0x43C10000;
+    const uint32_t cAxiClock      = 50e6;       // 50 [MHz]
+
+    typedef struct axiVideoCtrlRegs_s {
+        uint32_t version;     // 0x00 -  RO  - Version register
+        uint32_t tpCtrl;      // 0x04 - R/W  - Test Pattern Control
+        uint32_t tpSize;      // 0x08 - R/W  - Test Pattern Size
+        uint32_t rxCtrl;      // 0x0C - R/W  - RX / Image Receiver Control
+        uint32_t rxSizeStat;  // 0x10 -  RC  - RX / Image Receiver Size Status
+        uint32_t rxFrameCnts; // 0x14 -  RO  - RX / Image Receiver Frame Counter
+    } axiVideoCtrlRegs_t;
+
+public:
+    rccVideoCtrl(void);
+    ~rccVideoCtrl(void);
+
+    bool isInitialized(void) { return (mRegs != NULL); };
+
+    int version(void);
+
+    int writeReg(uint8_t regOffset, uint32_t regValue);
+    uint32_t readReg(uint8_t regOffset);
+    void dumpRegs(void);
+
+private:
+    int      cleanup(void);
+
+    int                         mMemFd;
+    volatile axiVideoCtrlRegs_t *mRegs;
+};
+
+#endif // __RCC_VIDEO_CTRL_H

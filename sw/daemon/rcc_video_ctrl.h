d307b268f1e9b107ff6f3b9701cabbf65de5777e
Jure Menart <juremenart@gmail.com>
2018-02-08 07:29:57 +0100

    added rxFifoStatus to video_ctrl register map

--- sw/daemon/rcc_video_ctrl.h
+++ sw/daemon/rcc_video_ctrl.h
@@ -13,14 +13,15 @@ private:
     const uint32_t cAxiClock        = 50e6;       // 50 [MHz]
 
     typedef struct axiVideoCtrlRegs_s {
-        uint32_t version;     // 0x00 -  RO  - Version register
-        uint32_t tpCtrl;      // 0x04 - R/W  - Test Pattern Control
-        uint32_t tpSize;      // 0x08 - R/W  - Test Pattern Size
-        uint32_t rxCtrl;      // 0x0C - R/W  - RX / Image Receiver Control
-        uint32_t rxSizeStat;  // 0x10 -  RC  - RX / Image Receiver Size Status
-        uint32_t rxFrameCnts; // 0x14 -  RO  - RX / Image Receiver Frame Counter
-        uint32_t rxFrameLen;  // 0x18 -  RO  - RX / Image Receiver Frame Length (can be used for framerate calculation)
-        uint32_t rxFifoCtrl;  // 0x1C - R/W  - RX FIFO control (when to start FLUSH to AXI-Stream, the line length)
+        uint32_t version;      // 0x00 -  RO  - Version register
+        uint32_t tpCtrl;       // 0x04 - R/W  - Test Pattern Control
+        uint32_t tpSize;       // 0x08 - R/W  - Test Pattern Size
+        uint32_t rxCtrl;       // 0x0C - R/W  - RX / Image Receiver Control
+        uint32_t rxSizeStat;   // 0x10 -  RC  - RX / Image Receiver Size Status
+        uint32_t rxFrameCnts;  // 0x14 -  RO  - RX / Image Receiver Frame Counter
+        uint32_t rxFrameLen;   // 0x18 -  RO  - RX / Image Receiver Frame Length (can be used for framerate calculation)
+        uint32_t rxFifoCtrl;   // 0x1C - R/W  - RX FIFO control (when to start FLUSH to AXI-Stream, the line length)
+        uint32_t rxFifoStatus; // 0x20 -  RO  - RX FIFO status (for debugging only currently)
     } axiVideoCtrlRegs_t;
 
 public:
